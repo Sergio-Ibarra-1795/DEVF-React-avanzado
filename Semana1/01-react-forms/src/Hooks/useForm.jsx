f4b2522e0a1fdb499c0cd5ba83b2938ffa6fe4be
Sergibar.1795 <sergio.ibarra1795@gmail.com>
2022-11-23 20:04:36 -0600

    Adding formulario en React parte 2

---
+++ Semana1/01-react-forms/src/Hooks/useForm.jsx
@@ -0,0 +1,39 @@
+// Reglas para crear un hook de React
+// Custom Hook es una función que va a utilizar otros Hooks de React
+// 1. Siempre debemos usar la palabra 'use' al nombrar el archivo
+// 2. Siempre debe n de ser funciones a partir de React v16 (que agrega funcioonalidad de hooks)
+// 3. Siempre deben de usar al menos un hook de React (useState,useEffect, useRef, etc)
+// 4. Deben ser reutilizables y no son hechos para casos especificos
+
+import { useState, useEffect } from 'react'
+
+function useForm (callback, defaults) {
+  // Estaod unico para guardar los datos de mi formulario en un objecto
+  const [input, setInput] = useState(defaults)
+
+  // Cargar valores por defecto
+  useEffect(() => {
+    setInput({ ...defaults })
+  }, [defaults])
+
+  // Funcion que se ejecuta cuando se escucha un cambio en el input
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    console.log(name, value)
+    setInput({ ...input, [name]: value })
+  }
+
+  // Funcion que se ejecuta al enviar el form
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    callback(input)
+  }
+
+  return {
+    input,
+    handleInputChange,
+    handleSubmit
+  }
+}
+
+export default useForm
--- Semana1/01-react-forms/src/components/SimpleForm.jsx
+++ Semana1/01-react-forms/src/components/SimpleForm.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-
+import { useState } from 'react'
 import logo from '../assets/react.svg'
 
 const SimpleForm = () => {
@@ -8,28 +7,29 @@ const SimpleForm = () => {
   const [password, setPassword] = useState('')
 
   const handleSubmit = (event) => {
+    // Evito que se recargue la pagina y rompa el SPA
     event.preventDefault()
-    const submitedData = JSON.stringify({
-      email,
-      password
-    })
-    console.log(submitedData)
+    const submittedData = JSON.stringify(
+      {
+        email,
+        password
+      })
+    console.log(submittedData)
   }
-
   // Paso 2: Crear el formulario base en JSX
   return (
     <div className='login'>
       <div className='login-container'>
         <img src={logo} alt='logo' />
         <form>
-
+          {/* Paso 3: Guardar cada cambio del input en su estado */}
           <label htmlFor='email'>Email</label>
           <input type='text' name='email' placeholder='correo@mail.com' id='email' onChange={(event) => setEmail(event.target.value)} />
 
           <label htmlFor='password'>Password</label>
           <input type='password' name='password' id='password' onChange={(event) => setPassword(event.target.value)} />
           <button onClick={handleSubmit}>
-            Iniciar sesion
+            Iniciar Sesion
           </button>
         </form>
       </div>
